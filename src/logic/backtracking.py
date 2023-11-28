142d9509c1cd01069a8fcf8dcf1aa54d5515ff68
Ivo Petrov <1414vo@gmail.com>
2023-11-28 17:55:00 +0000

    Implemented selective backtracker

--- src/logic/backtracking.py
+++ src/logic/backtracking.py
@@ -10,6 +10,7 @@ from src.logic.base_logic import BaseBacktracker
 from src.exceptions import InvalidBoardException
 from src.solver.board import Board
 import copy
+import numpy as np
 
 
 class NaiveBacktracker(BaseBacktracker):
@@ -51,3 +52,45 @@ class NaiveBacktracker(BaseBacktracker):
                     return True
 
         return False
+
+
+class SelectiveBacktracker(BaseBacktracker):
+    """! A class for improved selection backtracking."""
+
+    def __init__(self, print_results: bool = False):
+        """! Creates a selective backtracker.
+
+        @param print_results - A configuration parameter on whether to print the step results.
+        """
+        super(SelectiveBacktracker, self).__init__(print_results)
+        self.name = "SelectiveBacktracker"
+
+    def step(self, board):
+        """! Attempts to make progress on the board. Attempts to guess a possibility on the most
+        defined cell, i.e. the cell with the least number of options
+
+        @param board - The board to attempt progress on.
+        @throws - InvalidBoardException if the backtracker finds a cell with no possibilities.
+
+        @return Whether the step succeeded.
+        """
+        # Obtain the number of possibilities per cell
+        n_possibilities = np.vectorize(len)(board.cell_possibilities)
+        n_possibilities = np.where(
+            (n_possibilities == 1) & (board.board != 0), 10, n_possibilities
+        )
+
+        # Get the indeces of the best cell.
+        i, j = np.unravel_index(n_possibilities.argmin(), (9, 9))
+
+        # If there is a cell with no possibilities, report the error.
+        if len(board.possibilities[i, j]) == 0:
+            raise InvalidBoardException("No option for number selection")
+
+        # Update the board with the new guess.
+        num = next(iter(board.cell_possibilities[i, j]))
+        self.board_memory.append(board.board.copy())
+        self.cell_pos_memory.append(copy.deepcopy(board.cell_possibilities))
+        self.guess_memory.append((i, j, num))
+        board.update(i, j, num)
+        self.print_msg(i + 1, j + 1, num, board)
